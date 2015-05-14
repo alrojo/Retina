4ea16a13d237ece85f8cd5b9dfbc6dd608088430
Morten <mortenbjelbo@gmail.com>
2015-05-14 22:02:31 +0200

    It should work now, I hope it does

--- Matlab/image_processing.py
+++ Matlab/image_processing.py
@@ -14,6 +14,7 @@ import multiprocessing
 import functools
 import pathos.multiprocessing as mp
 import PIL.Image
+import getopt
 # from joblib import Parallel, delayed # got ditched to pathos
 
 
@@ -21,12 +22,12 @@ def do_nothing_fun(input):
     return input
 
 
-def image_load(path):
+def image_load((path,name)):
     image = skimage.io.imread(path)
-    return path, image
+    return path, name, image
 
 
-def image_save((image, name, path)):
+def image_save((path, name, image)):
     save_as = os.path.join(path, name)
     save_as = os.path.relpath(save_as)
     skimage.io.imsave(save_as, image)
@@ -38,17 +39,18 @@ def get_name_image(image_path):
     return name
 
 
-def flatten_hist(img):
+def flatten_hist(path, name, img):
     hist, bins = np.histogram(img.flatten(), 256, [0, 256])
     cdf = hist.cumsum()
     cdf_m = np.ma.masked_equal(cdf, 0)
     cdf_m = (cdf_m - cdf_m.min())*255.0/(cdf_m.max()-cdf_m.min())
     cdf = np.ma.filled(cdf_m, 0).astype('uint8')
     img = cdf[img]
-    return img
+    return path, name, img
 
 
-def remove_background_noise(image):
+
+def remove_background_noise(path, name, image):
     """Watch out, this function transforms inplace"""
     h, w = image.shape[:2]
     ma = np.mean(image, axis=2)
@@ -56,7 +58,7 @@ def remove_background_noise(image):
     threshold = max(np.max(0.3*np.median(v)), 0.75*np.min(v))
     mask = (ma < threshold)
     image[mask] = 0
-    return image
+    return path, name, image
 
 
 def convert_to_uin8(image):
@@ -114,7 +116,7 @@ def from_rgb_to_hsv(im):
     return skimage.color.convert_colorspace(im, 'RGB', 'HSV')
 
 
-def pil_resize(image_path, image_save_path, size):
+def pil_resize((image_path, image_save_path, size)):
     name = get_name_image(image_path)
     im = PIL.Image.open(image_path)
     im.thumbnail((size, size), PIL.Image.LANCZOS)
@@ -132,6 +134,7 @@ def pil_resize(image_path, image_save_path, size):
     name = name.replace("jpeg", "png")
     outfile = os.path.join(image_save_path, name)
     im.save(outfile)
+    return image_save_path, name
 
 
 # These functions allow for a pipelining of the above
@@ -141,25 +144,75 @@ def transform_pipeline(*functions):
     return functools.reduce(lambda f, g: lambda x: f(g(x)), functions)
 
 
-def main():
-    global development
-    development = True
-    image_out_id = "512_with_special_sauce"
-    directory = '/home/morten/Git_and_dropbox_not_friends/Retina/sample'
-    save_out = os.path.join(directory, image_out_id)
-    if not os.path.exists(save_out):
-        os.makedirs(save_out, mode=0755)
+def main(path_to_images, path_out):
+    directory = path_to_images
+    if not os.path.exists(path_out):
+        os.makedirs(path_out, mode=0755)
     image_names = glob.glob(os.path.join(directory, "*.jpeg"))
     if (len(image_names) == 0):
         image_names = glob.glob(os.path.join(directory, "*.png"))
     image_names.sort
-
+    arg1 = image_names
+    arg2 = len(arg1)*[path_out]
+    # Note, this is also hardcoded for now
+    size = 1024
+    arg3 = len(arg1)*[size]
+    arguments = zip(arg1, arg2, arg3)
     num_cores = multiprocessing.cpu_count()
-    transform_image = transform_pipeline(image_load, image_save)
+    # Edit this!!  Function should be specified first function first
+    # and last function last
+    # You need to make sure the the combination of functions are
+    # valid when it comes to outputs and inputs
+    # also in transform_image first function should have the arguments fed by
+    # i in p.map
+
+    # here are some examples
+
+    # example 1.  just scales
+    # transform_image = transform_pipeline(pil_resize(directory, save_out, 1024))
+    # example 2.  scales and crops
+
+    # pil_resize needs ((image_path, image_save_path, size)): and returns image_path
+    # image_load needs (path) and returns path, image
+    # remove_background_noise needs (image) and returns image
+    # flatten_hist needs img and return img
+    # image_save needs ((image, name, path)) and returns 0
+
+    # So here there is a problem of mismatch between image_load and remove_background
+    # between flatten and save
+
+    # first problem is fixed by deleting the path return on image_load, but then again
+    # then it can't be passed to image_save, therefore remove background and flatten hist 
+    # are upgraded to take the path too.  Also name needs to passed all the way through 
+    # from pilresize that renames it
+
+     # This is now done
+    # pil returns image_save_path, name  # because the rest of these function should 
+    # done inplace
+    # image_load loads tuple (image_path, name) and returns return path, name, image
+    # remove_back... takes tuple (path, name, image) and returns the same
+    # flatten takes tuple (path, name, image) and returns path, name, img
+    # image_save takes tuple (path, name, img) which should be the correct path 
+    # as pil retuned image_save_path
+
+
+
+    transform_image = transform_pipeline(pil_resize,
+                                         image_load,
+                                         remove_background_noise,
+                                         flatten_hist,
+                                         image_save)
     p = mp.Pool(num_cores)
-    p.map(lambda i: transform_image(i), image_names)
+    p.map(lambda i: transform_image(i), arguments)
 
     return 0
+
+
+def usage():
+    print "Supply path to image folder and path to where you want them saved"
+    print ("I didn't get around to automatically pass the functions you want"
+           "to use. So you need to hardcode them in main"
+           "--edit the *transform_image* variable")
 
 
 # Misc. function I might not use
@@ -177,7 +230,10 @@ def return_zero(input):
 
 
 if __name__ == '__main__':
-    program = main()
+    if len(sys.argv) < 3:
+        print usage()
+    #path_to_images, path_out
+    program = main(sys.argv[1], sys.argv[2])
 
 
 ####################################################
@@ -189,7 +245,9 @@ notes = """
 NOTES:
     - The important info I should bundle up and pass around as a dict
     - I should make a function that can run through all the images and make sure they
-        - One function to make sure they are not rotated.
+        - One function to make sure they are not rotated. 
+          Apparently 15840_left.jpeg in the test is wrongly rotated.
+          Actually, I don't think I care, just remember to do a least one scaling first
         - One function to make sure that after resizing they all have the same dimensions
     - add arguments and usage()
 # TODO: see http://matthewrocklin.com/blog/work/2013/12/05/Parallelism-and-Serialization/
